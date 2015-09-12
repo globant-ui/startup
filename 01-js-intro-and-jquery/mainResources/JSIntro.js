d34f65d7d50800f97e7cba26da59f3fc848f74e0
Nahuel Alejandro Veron <nahuelav96@gmail.com>
2015-09-12 01:10:52 -0300

    Refactorize some CSS Exercises.
    Finish Topic 1 Exercises

---
+++ 01-js-intro-and-jquery/mainResources/JSIntro.js
@@ -0,0 +1,93 @@
+// Will Run on DOM Ready!
+$(document).ready(function() {
+    // When the page has finished loading the section must fade in.
+    // Topic 1, exercise 5
+
+    // Add a textbox with the class "alias", and put the cursor inside it right after the section fades in.
+    // Function will focus when fade in completes.
+    // Topic 1, exercise 6
+
+    $('#hiddenSection').fadeIn(400, function()
+    {
+        $('.alias').focus();
+    });
+
+    //Attach an event to the created button which calls a function that gets a response from (...)
+    // Topic 1, exercise 7
+    $('#responseButton').click(getResponse);
+});
+
+// Will Handle Response Errors
+function handleError(xhr, status, error)
+{
+    // jQuery Target
+    var targetText = $('#targetText');
+
+    // Clean element classes
+    targetText.removeClass();
+
+    // Add Failed Class
+    targetText.addClass('failed');
+
+    // Add Text on Target
+    targetText.text(xhr.responseText);
+}
+
+// Will Handle a successful response
+function sucessfulResponse (data)
+{
+    // jQuery Target
+    var targetText = $('#targetText');
+
+    // Check for errors
+    if (data['error'])
+    {
+        var xhrerr = {responseText: data['error']};
+        return handleError(xhrerr, status, '');
+    }
+    // Check if data has no response
+    else if (!data['response'])
+    {
+        var xhrnull = {responseText: 'Response is Invalid!'};
+        return handleError(xhrnull, status, '');
+    }
+    else
+    {
+        // Clean element classes
+        targetText.removeClass();
+
+        // Add Failed Class
+        targetText.addClass('success');
+
+        // Add Text on Target
+        targetText.text(data.response);
+
+        /* Mark name */
+        var html = targetText.html();
+        html = html.substring(0, 8) + '<mark>' + html.substring(8) + '</mark>';
+        targetText.html(html)
+    }
+}
+
+// On Button Click
+function getResponse()
+{
+    // Get the name of the user!
+    var name = $('.alias').val();
+
+    // Url of webserver
+    var url = 'http://bootcamp.aws.af.cm/welcome/';
+
+    // Get the response
+    $.ajax({
+        type: 'GET',
+        url: ((name != null) && (name != undefined)) ? url + name : name,
+        async: true,
+        data: { get_param: 'response' },
+        dataType: 'json',
+        error: handleError,
+        success: sucessfulResponse
+    });
+}
+
+
---
+++ 01-js-intro-and-jquery/mainResources/Spotify.js
@@ -0,0 +1,64 @@
+// Will Run on DOM Ready!
+$(document).ready(function()
+{
+    $('#search').click(search);
+});
+
+// Handle a good response from ajax
+function manageResponse(data)
+{
+
+    // Get all items
+    $.each(data['albums']['items'], function(index, value)
+    {
+        var release = '';
+        // Get Ajax for release date, we dont use async for save a variable directly w/o callback!
+        $.ajax({
+            type: 'GET',
+            url: value['href'],
+            async: false,
+            dataType: 'json',
+            error: function(fstp, status, error){
+                //console.log('err:' + fstp.responseText);
+            },
+
+            success: function (response) {
+                release = response['release_date']
+            }
+        });
+
+        // Html Element Target
+        var htmlTarget = $('#result');
+        htmlTarget.html(htmlTarget.html() +
+        '<article id="result" class="box">' +
+            '<div class="img-container">' +
+                '<img src="' + value['images'][2]['url'] + '" alt="' + value['name'] + '"/>' +
+            '</div>' +
+            '<div class="content-container">' +
+                '<p><strong>' + value['name'] + '</strong></p>' +
+                '<p><em>' + value['album_type'] + ', ' + release + '</em></p>' +
+                '<p><a href="' + value['external_urls']['spotify'] + '" target="_blank">See it on Spotify!</a></p>' +
+            '</div>' +
+        '</article>')
+
+    })
+}
+
+function manageError()
+{
+
+}
+
+function search()
+{
+    $('#result').html('');
+    // Get the response
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.spotify.com/v1/search?q=' + $('#searchData').val() + '&type=album',
+        async: true,
+        dataType: 'json',
+        error: manageError,
+        success: manageResponse
+    });
+}
