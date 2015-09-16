ca177c9ddac7d10fffa4825d7405744916409263
Nahuel Veron <nahuelav96@gmail.com>
2015-09-16 13:15:13 -0300

    Testing Handlebars templating engine #1

---
+++ 04-backbone/Modules/EnumGenres.js
@@ -0,0 +1,21 @@
+var Genres =
+{
+    "Action"        : "Action",
+    "Adventure"     : "Adventure",
+    "Animation"     : "Animation",
+    "Comedy"        : "Comedy",
+    "Crime"         : "Crime" ,
+    "Documentary"   : "Documentary",
+    "Drama"         : "Drama",
+    "Fantasy"       : "Fantasy",
+    "History"       : "History",
+    "Horror"        : "Horror",
+    "Musical"       : "Musical" ,
+    "Mistery"       : "Mistery",
+    "Romance"       : "Romance" ,
+    "Sci-fi"        : "Sci-fi",
+    "Thriller"      : "Thriller",
+    "Western"       : "Western"
+};
+
+module.exports = Genres;
--- 04-backbone/index.js
+++ 04-backbone/index.js
@@ -1,3 +1,19 @@
-/**
- * Created by negan on 15/09/2015.
- */
+var $ = require('jquery');
+var Handlebars = require('handlebars');
+
+var Genres = require('./Modules/EnumGenres');
+var MovieModel = require('./Modules/MovieModel');
+
+var Movie = new MovieModel();
+Movie.setDesc('DDDFF');
+Movie.setGenre(Genres.Action);
+Movie.setTitle('TITLE');
+Movie.setYear('1999');
+Movie.setImgUrl('https://placeholdit.imgix.net/~text?txtsize=24&txt=256%C3%97256&w=256&h=256');
+var scriptID = $('#movieData').html();
+var template = Handlebars.compile(scriptID);
+
+$(document).ready(function()
+{
+    $(document.body).html(template(Movie));
+});
