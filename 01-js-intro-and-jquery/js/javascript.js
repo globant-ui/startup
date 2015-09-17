366186b34173e9e1b280c31d0fb9132e2d9608b9
Demian-Moschin <demianmoschin@gmail.com>
2015-09-17 14:48:12 -0300

    [Demian Moschin]- topic 01 - Issues brought up in comments,  fixed

--- 01-js-intro-and-jquery/js/javascript.js
+++ 01-js-intro-and-jquery/js/javascript.js
@@ -1,84 +1,55 @@
-
-
+//Module Patter Function
 var functionFadeToggleSection = (function() {
   //Private variables
-  var selectedSection = $('section.hidden-section');
-  var text = $('input.alias');
-
-  var callBackFunction = function() {
-    text.trigger('focus'); //only in chrome?
-    
+  var selectedSection = $('.hidden-section');
+  var searchInputText = $('input.alias');
+  //Private functions
+  var focusSearchInputText = function() {
+    searchInputText.trigger('focus'); //only in chrome?
   };
-
-  var ft = function() {
-    selectedSection.fadeIn("slow",callBackFunction);
+  var fadeInSection = function() {
+    selectedSection.fadeIn('slow', focusSearchInputText);
   };
-  
   //public API
   return {
-    fadeAndToogle: ft
-
+    fadeAndToogle: fadeInSection
   };//end return 
-  
 })();
 
-
-var removeClassFunction = (function() {
-  //Private variables
-  var section = $('section.hidden-section');
-
-    var remove = function() {
-        section.removeClass('hidden-section');
-    };
-  
-  //public API
-  return {
-    remove: remove
-
-  };//end return 
-  
-})();
-
-
+//Module Patter Function
 var delegateFunction =  (function() {
   //Private variables
   var obj = $('.button-delegates');
   var eventType = 'click';
   var selectedSection = $('section.hidden-section');
   var ajaxReturn ;
-
-  var insertHtmlWithAjax = function(){ 
-    $.ajax( "http://bootcamp.aws.af.cm/welcome/yourname" )
+  //Private functions
+  var insertHtmlWithAjax = function() { 
+    $.ajax( 'http://bootcamp.aws.af.cm/welcome/yourname' )
 
     .done(function(result) {
     ajaxReturn = result.response;
-    selectedSection.append("<p>" + ajaxReturn + "</p>");
+    selectedSection.append('<p>' + ajaxReturn + '</p>');
     })
 
     .always(function() {
-      alert("delegateFunction working");
+      alert('delegateFunction working');
     })
 
     .fail(function() {
-      alert( "error in section. Red color" );
+      alert( 'error in section. Red color' );
     })
- 
   };//end jqxhr
 
   var functionDelegate = function() {
     obj.delegate(obj, eventType, insertHtmlWithAjax); 
   };
-
   //public API
   return {
     delegate: functionDelegate
   };//end return 
-  
 })();
 
-/*--------- finished ones -----------------*/
-
-
 //Passing a named function instead of an anonymous function 
-$(document).ready(functionFadeToggleSection.fadeAndToogle());
-$(document).ready(delegateFunction.delegate());
+functionFadeToggleSection.fadeAndToogle();
+delegateFunction.delegate();
--- 01-js-intro-and-jquery/js/spootify.js
+++ 01-js-intro-and-jquery/js/spootify.js
@@ -1,51 +1,47 @@
-var dataAPI =  (function() {
-  //Private variables
-  var selectedSection = $('article.albums');
-
-  //Private methods
-  var searchAlbum = function(artist){ 
-
-    $.ajax({
-          url: 'https://api.spotify.com/v1/search',
+var spootifyAjaxResponse = function(paramUrl,paramDataTypeSearch,paramArtist) {
+var htmlResult = '';
+ $.ajax({
+          url: paramUrl,
           dataType: 'json', // ** added in order to iterate with $.each **
           data: {
-            q: artist,
-            type: 'album'
+            q: paramArtist,
+            type: paramDataTypeSearch
           },//data
 
           success: function(data) {
             $.each(data.albums.items, function(index,item) {
                 console.log(item);
-                selectedSection.append("<p>" + item.name + "</p> " );
-                selectedSection.append("<p><img src=" + item.images[1].url + " ></p> ");
-                selectedSection.append("<p>" + item.uri + "</p>");
+                htmlResult = htmlResult + ("<p>" + item.name + "</p> <p><img src=" + item.images[1].url + " ></p><p>" + item.uri + "</p>");
               }); //.each
           }, //success
 
           error: function(XMLHttpRequest, textStatus, errorThrown) {
               alert("Errors, trying to get the Stones" + errorThrown + textStatus );
           }//error
-
     })//jQuery.ajax
+              
+  return htmlResult;           
 
-    .done(function(result) {
-        alert(artist + " albums !")
-    })//done 
+};
 
+//Module Patter Function
+var dataAPI =  (function() {
+  //Private variables
+  var selectedSection = $('article.albums');
+  var htlmResult = '';
+  //Private methods
+  var searchAlbum = function(artist) { 
+    htlmResult = spootifyAjaxResponse('https://api.spotify.com/v1/search','album',artist);
+    selectedSection.append(htlmResult);   
   };//end insertHtmlWithAjax
-
 
   //public API
   return {
     searchResult: searchAlbum
   };//end return 
-  
 })();
 
-
-
-
-
+//Module Patter Function
 var spootifySearch =  (function() {
   //Private variables
   var obj = $(".button-artists");
@@ -55,24 +51,18 @@ var spootifySearch =  (function() {
   var getData = function() {
     var artist;
     artist = document.getElementById("search-input").value;
-    if (artist) {dataAPI.searchResult(artist); };
-    
+    if (artist) {
+      dataAPI.searchResult(artist);
+       };//end if
   };
-
   var functionDelegate = function() {
     obj.delegate(obj, eventType, getData); 
   };
-
   //public API
   return {
     searchInSpootify: functionDelegate
   };//end return 
-  
 })();
 
-
-
-
 //Passing a named function instead of an anonymous function 
-
-$(document).ready(spootifySearch.searchInSpootify());
+spootifySearch.searchInSpootify();
