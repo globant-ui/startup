a4c677d5652adf7082fd0cddce79faa5fa13944b
Nahuel Alejandro Veron <nahuelav96@gmail.com>
2015-09-21 20:23:31 -0300

    Finished services for get from localstorage & generation of movie object

--- 05-angularjs/dist/js/controllers.js
+++ 05-angularjs/dist/js/controllers.js
@@ -2,20 +2,55 @@ var ctrls = angular.module('startupControllers', ['ngStorage']);
 
 
 // Movie Editor Controller
-ctrls.controller('MovieEditorCtrl', ['$scope', '$routeParams', '$window', '$localStorage',
-  '$timeout', 'LocalStorageService',
-  function ($scope, $routeParams, $window, $localStorage, $timeout, LocalStorageService )
+ctrls.controller('MovieEditorCtrl', ['$scope', '$routeParams', '$window',
+  '$timeout', 'LocalStorageService', 'MovieGenerator',
+  function ($scope, $routeParams, $window,  $timeout, LocalStorageService, MovieGenerator )
 {
-  // Get Action Parameters
+  // Get Action and idParams
   $scope.action = $routeParams.action;
-  // Get Param id
   $scope.movieId = $routeParams.id;
+
   // Bind data to controller from LocalStorage
-  $scope.data = $localStorage.$default({movies:[]});
+  $scope.movies = LocalStorageService.ReadAllMovies();
+
+  // We use a transitory item because don't want a realtime-binding and only want to save when user
+  // press 'Save' and the content is valid!.
+  $scope.tItem = (($scope.movieId !== undefined)&&($scope.movieId < LocalStorageService.ReadAllMovies().length)) ?
+  MovieGenerator.GenerateFromMovie(LocalStorageService.ReadMovie($scope.movieId)) : MovieGenerator.GenerateEmpty();
 
   // Action to clicks, apply on View
   $scope.editClick = function() { $window.location.href = './#/edit/' + $scope.movieId; };
   $scope.delClick = function() { $window.location.href = './#/del/' + $scope.movieId; };
+
+  // Update a Movie
+  $scope.editItem = function (state)
+  {
+    // Validate Only if is true
+    if (state === true)
+    {
+      // Pass Transitory Item to data
+      LocalStorageService.UpdateMovie($scope.movieId, $scope.tItem);
+
+      // Back To View, with a timeout of 200msec to finish saving
+      $timeout(function(){
+        $window.location.href = './#/view/' + $scope.movieId;
+      },200);
+    }
+  };
+
+  // Create a Movie
+  $scope.addItem = function (state)
+  {
+    // Add only if is valid
+    if (state === true)
+    {
+      // Push item to Data
+      LocalStorageService.CreateMovie($scope.tItem);
+
+      // Go Back to List
+      $window.location.href = './';
+    }
+  };
 
   // Check if action is delete, it will automatically delete the item
   if ($scope.action === 'del')
@@ -25,73 +60,11 @@ ctrls.controller('MovieEditorCtrl', ['$scope', '$routeParams', '$window', '$loca
     // Redirect to List
     $window.location.href = './';
   }
-
-  // Check if action is edit, and define needed methods and fields
-  else if ($scope.action === 'edit')
-  {
-    // We use a transitory item because don't want a
-    // realtime-binding and only want to save when user
-    // press 'Save' and the content is valid!.
-    $scope.tItem =
-    {
-      title : $scope.data.movies[$scope.movieId].title,
-      year  : $scope.data.movies[$scope.movieId].year,
-      genre : $scope.data.movies[$scope.movieId].genre,
-      desc  : $scope.data.movies[$scope.movieId].desc,
-      imgUrl: $scope.data.movies[$scope.movieId].imgUrl
-    };
-
-    // Define editItem Method
-    $scope.editItem = function (state)
-    {
-      // Validate Only if is true
-      if (state === true)
-      {
-        // Pass Transitory Item to data
-        LocalStorageService.UpdateMovie($scope.movieId, $scope.tItem);
-
-        // Back To View, with a timeout of 200msec to finish saving
-        $timeout(function(){
-          $window.location.href = './#/view/' + $scope.movieId;
-        },200);
-      }
-    };
-  }
-
-  // Check if action is add, and define needed methods and fields
-  else if ($scope.action === 'add')
-  {
-    // We use a transitory item because don't want a
-    // realtime-binding and only want to save when user
-    // press 'Save' and the content is valid!.
-    $scope.tItem =
-    {
-      title : '',
-      year  : 0,
-      genre : '',
-      desc: '',
-      imgUrl: 'http://img2.wikia.nocookie.net/__cb20130819212538/dragon-stones/images/f/fc/256x256.gif'
-    };
-
-    // Define addItem Method
-    $scope.addItem = function (state)
-    {
-      // Add only if is valid
-      if (state === true)
-      {
-        // Push item to Data
-        LocalStorageService.CreateMovie($scope.tItem);
-
-        // Go Back to List
-        $window.location.href = './';
-      }
-    };
-  }
 }]);
 
 // Movie List Controller
 ctrls.controller('MovieListCtrl', ['$scope', 'LocalStorageService', function ($scope, LocalStorageService)
 {
   // Bind data to controller from LocalStorage
-  $scope.movies = LocalStorageService.ListAllMovies();
+  $scope.movies = LocalStorageService.ReadAllMovies();
 }]);
--- 05-angularjs/dist/js/routing.js
+++ 05-angularjs/dist/js/routing.js
@@ -1,81 +1,95 @@
-var app = angular.module('startupMovies', ['ngRoute', 'ngStorage',  'startupControllers']);
+var app = angular.module('startupMovies', ['ngRoute', 'ngStorage', 'startupControllers']);
 
 // Routing
-app.config(['$routeProvider',function ($routeProvider)
-  {
-    $routeProvider
+app.config(['$routeProvider', function ($routeProvider)
+{
+  $routeProvider
     // Routing for all urls that follow the next format: ./xxxx/xx (./edit/1, ./del/1 ).
     .when('/:action/:id', {
       templateUrl: 'dist/Views/movieDetailsView.html',
-      controller: 'MovieEditorCtrl'
+      controller : 'MovieEditorCtrl'
     })
     // Routing for all urls that follow the next format: ./xxxx (./add ).
     .when('/:action', {
       templateUrl: 'dist/Views/movieDetailsView.html',
-      controller: 'MovieEditorCtrl'
+      controller : 'MovieEditorCtrl'
     })
     // Route by default, shows the movie list
     .when('/', {
-        templateUrl: 'dist/Views/movieListView.html',
-        controller: 'MovieListCtrl'
+      templateUrl: 'dist/Views/movieListView.html',
+      controller : 'MovieListCtrl'
     })
     // If route is invalid, redirect to default route.
     .otherwise({
-        redirectTo: '/'
-      });
-  }]);
+      redirectTo: '/'
+    });
+}]);
 
 // Preload Templates using templateCache
-app.run(['$templateCache', '$http', function($templateCache, $http) {
+app.run(['$templateCache', '$http', function ($templateCache, $http)
+{
   $http.get('dist/Views/movieListView.html', {cache: $templateCache});
   $http.get('dist/Views/movieDetailsView.html', {cache: $templateCache});
 }]);
 
 // Service that manage Local Storage
-app.service('LocalStorageService',['$localStorage', function($localStorage) {
-  var data = $localStorage.$default({movies:[]});
-
-  this.ListAllMovies = function()
+app.service('LocalStorageService', ['$localStorage', function ($localStorage)
+{
+  // Load LocalStorage, if it can't, it will return a object with an empty property
+  // called 'movies'.
+  var data = $localStorage.$default({movies: []});
+  // Return the full array with all movies.
+  this.ReadAllMovies = function ()
   {
     return data.movies;
   };
+  // C.R.U.D. Methods:
+
+  // Create a movie into the array
   this.CreateMovie = function (movie)
   {
     data.movies.push(movie);
   };
-  this.ReadMovie = function(index)
+  // Read a movie (by index) from the array
+  this.ReadMovie = function (index)
   {
-    if (arguments[0] !== undefined)
-    {
-      return data.movies[index];
-    }
-    else
-    {
-      return undefined;
-    }
+    return data.movies[index];
   };
-  this.UpdateMovie = function(index, movie)
+  // Update a movie (by index) from the array
+  this.UpdateMovie = function (index, movie)
   {
     data.movies[index] = movie;
   };
-  this.DeleteMovie = function(index)
+  // Delete a Movie (by index) from the array
+  this.DeleteMovie = function (index)
   {
     data.movies.splice(index, 1);
   };
 }]);
 
 // Service that generates a empty (or with data )
-app.service('MovieGenerator', function()
+app.service('MovieGenerator', function ()
 {
-  this.Generate = function()
+  // Generate an empty movie object
+  this.GenerateEmpty = function ()
   {
     return {
-      title:  (arguments[0].title   !== undefined) ? arguments[0].title   : '',
-      year:   (arguments[0].year    !== undefined) ? arguments[0].year    : '',
-      genre:  (arguments[0].genre   !== undefined) ? arguments[0].genre   : '',
-      desc:   (arguments[0].desc    !== undefined) ? arguments[0].desc    : '',
-      imgUrl: (arguments[0].imgUrl  !== undefined) ? arguments[0].imgUrl  :
-        'http://img2.wikia.nocookie.net/__cb20130819212538/dragon-stones/images/f/fc/256x256.gif'
+      title : '',
+      year  : '',
+      genre : '',
+      desc  : '',
+      imgUrl: 'http://img2.wikia.nocookie.net/__cb20130819212538/dragon-stones/images/f/fc/256x256.gif'
+    };
+  };
+  // Generate a new movie object from an existent movie
+  this.GenerateFromMovie = function (movie)
+  {
+    return {
+      title : movie.title,
+      year  : movie.year,
+      genre : movie.genre,
+      desc  : movie.desc,
+      imgUrl: movie.imgUrl
     };
   };
 });
