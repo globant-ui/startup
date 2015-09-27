741705e57aa2d20000154b5f8ff03a0f80382232
chino1504 <c.c.sanchez@gmail.com>
2015-09-27 20:11:58 -0300

    more movies

--- 05-angularjs/scripts/controllers.js
+++ 05-angularjs/scripts/controllers.js
@@ -1,39 +1,3 @@
-/*myApp.controller('MainCtrl', ['$scope', function MainCtrl($scope, $http) {
-  $scope.movies = [{
-     'title': 'Star Wars',
-    'year': '1977',
-    'description': 'Luke Skywalker joins forces with a Jedi Knight, \
-    a cocky pilot, a wookiee and two droids to save the universe \
-    from the Empire\'s world-destroying battle-station, while also \
-    attempting to rescue Princess Leia from the evil Darth Vader.',
-    'genre': 'Fantasy',
-    'director': 'George Lucas'
-    },
-     {
-    'title': 'Terminator 2',
-    'year': '1991',
-    'description': 'A cyborg, identical to the one who failed \
-    to kill Sarah Connor, must now protect her young son, \
-    John Connor, from a more advanced cyborg, made out of \
-    liquid metal.',
-    'genre': 'Action',
-    'director': 'James Cameron'
-    },
-     {
-    'title': 'Back to the Future',
-    'year': '1985',
-    'description': 'A young man is accidentally sent 30 years \
-    into the past in a time-traveling DeLorean invented by \
-    his friend, Dr. Emmett Brown, and must make sure his \
-    high-school-age parents unite in order to save his own \
-    existence.',
-    'genre': 'Adventure',
-    'director': 'Robert Zemeckis'
-    }
-  ];
-});*/
-
-
 
 movieList.controller('AppCtrl', function AppCtrl ($scope) {
     $scope.movies = [{
@@ -45,7 +9,29 @@ movieList.controller('AppCtrl', function AppCtrl ($scope) {
     attempting to rescue Princess Leia from the evil Darth Vader.',
     genre: 'Fantasy',
     director: 'George Lucas'
-    }];
+    },
+    {
+      title: 'Terminator 2',
+      year: '1991',
+      description: 'A cyborg, identical to the one who failed \
+    to kill Sarah Connor, must now protect her young son, \
+    John Connor, from a more advanced cyborg, made out of \
+    liquid metal.',
+    genre: 'Action',
+    director: 'James Cameron'
+    },
+     {
+      title: 'Back to the Future',
+      year: '1985',
+      description: 'A young man is accidentally sent 30 years \
+    into the past in a time-traveling DeLorean invented by \
+    his friend, Dr. Emmett Brown, and must make sure his \
+    high-school-age parents unite in order to save his own \
+    existence.',
+    genre: 'Adventure',
+    director: 'Robert Zemeckis'
+    }
+    ];
   });
 
 movieList.controller('InfoCtrl',
