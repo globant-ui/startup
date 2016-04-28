ab923ea90acaacaacccb580037adc70bd0fdfba1
Jacobo Dominguez <dominguezjaco@gmail.com>
2016-04-28 19:45:48 -0300

    added addCast method for arrays or single objects

--- 02-oop-inheritance/movies.js
+++ 02-oop-inheritance/movies.js
@@ -24,6 +24,7 @@ class Movie extends EventEmitter{
     this.title = title
     this.year = year
     this.duration = duration
+    this.cast = []
   }
 
 
@@ -37,6 +38,24 @@ class Movie extends EventEmitter{
 
   resume(){
     this.emit("resume")
+  }
+
+  addCast(cast){
+    let self = this
+    if(Array.isArray(cast)){
+      cast.forEach(function(actor){
+        self.cast.push(actor)
+      })
+    }else{
+      this.cast.push(cast)
+    }
+  }
+}
+
+class Actor {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
   }
 }
 
@@ -61,8 +80,13 @@ let social = {
 
 let movie1 = new Movie("Batman inicia", "2005", 120);
 let logger = new Logger();
-let movie2 = new Movie("Spiderman 3", "2007", 150);
-let movie3 = new Movie("The Avengers", "2012", 130);
+
+let cristianBale = new Actor("Christian Bale", 30);
+let otherCast = [
+ new Actor('Morgan Freeman', 40),
+ new Actor('Gary Oldman', 25),
+ new Actor('Katie Holmes', 37)
+];
 
 movie1.on("play", logger.log);
 movie1.on("resume", logger.log);
@@ -74,4 +98,9 @@ movie1.pause();
 let batmanInicia = Object.assign(movie1, social);
 
 batmanInicia.share("Juancito Perez");
-batmanInicia.like("");
+batmanInicia.like("Pedro Juarez");
+
+batmanInicia.addCast(cristianBale);
+batmanInicia.addCast(otherCast);
+
+console.log(movie1.cast);
