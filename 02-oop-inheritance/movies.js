c17d9405df4d07b5dd5dfb3c67e2c515c8c86437
Jacobo Dominguez <dominguezjaco@gmail.com>
2016-04-27 21:13:39 -0300

    movies with eventemmiter and logger

--- 02-oop-inheritance/movies.js
+++ 02-oop-inheritance/movies.js
@@ -1,45 +1,63 @@
-class Movie {
+class EventEmitter {
+
+  constructor() {
+    this.events = {
+    }
+  }
+
+  on(event, callback){
+    this.events[event] = callback
+  }
+
+  emit(event){
+    this.events[event](event)
+  }
+
+  off(event){
+    delete this.events[event]
+  }
+}
+
+class Movie extends EventEmitter{
   constructor(title, year, duration) {
+    super()
     this.title = title
     this.year = year
     this.duration = duration
   }
 
+
   play(){
-    console.log("play: " + this.title);
+    this.emit("play")
   }
 
   pause(){
-    console.log("pause: " + this.year);
+    this.emit("pause")
   }
 
   resume(){
-    console.log("duration: " + this.year);
+    this.emit("resume")
   }
 }
 
-let movie1 = new Movie("Batman inicia", "2005", 120);
-let movie2 = new Movie("Spiderman 3", "2007", 150);
-let movie3 = new Movie("The Avengers", "2012", 130);
-
-movie1.play();
-
-class EventEmitter {
-  this.listener
-
+class Logger {
   constructor() {
 
   }
 
-  on(listener){
-    this.listener = listener
-  }
-
-  emit(class){
-    
-  }
-
-  off(){
-    this.listener = null
+  log(info){
+    console.log("output: The " + info + " event has been emitted")
   }
 }
+
+let movie1 = new Movie("Batman inicia", "2005", 120);
+let logger = new Logger();
+let movie2 = new Movie("Spiderman 3", "2007", 150);
+let movie3 = new Movie("The Avengers", "2012", 130);
+
+movie1.on("play", logger.log);
+movie1.on("resume", logger.log);
+movie1.on("pause", logger.log);
+movie1.play();
+movie1.resume();
+movie1.pause();
