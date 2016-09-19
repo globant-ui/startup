3f0f085b55a8a0cc1d9c74eade55438885e33b8c
guillermina <guilleves@gmail.com>
2016-09-19 17:58:30 -0300

    [Guillermina Vescovo] - Exercise 10 - Complete T01E10, nevermind the layout

--- 01-JS-DOM-APIs/assets/scripts.js
+++ 01-JS-DOM-APIs/assets/scripts.js
@@ -9,16 +9,45 @@ function fadeIn(){
 }
 
 // exercise 6
-function getJoke() {
+// function getJoke() {
+//   let xhr = new XMLHttpRequest();
+//   xhr.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//         let response = JSON.parse(this.responseText).value.joke;
+//         // how about appendChild instead of innerHTML?
+//         document.querySelector("p.joke").innerHTML = "";
+//         document.querySelector("p.joke").innerHTML += response;
+//     }
+//     else if (this.status >= 500 ) {
+//         document.querySelector("section").style.color = 'red';
+//     }
+//   };
+//   xhr.open("GET", "https://api.github.com/search/repositories", true);
+//   xhr.send();
+// }
+//     let node = document.createElement("li");
+    // let textNode = document.createTextNode(element.full_name);
+    // node.appendChild(textNode);
+    // document.getElementById("repo-list").appendChild(node);
+
+function hitAPI() {
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        let response = JSON.parse(this.responseText).value.joke;
-        // how about appendChild instead of innerHTML?
-        document.querySelector("p.joke").innerHTML = "";
-        document.querySelector("p.joke").innerHTML += response;
+        var repositories = JSON.parse(this.responseText);
+        useResponse(repositories);
     }
   };
-  xhr.open("GET", "http://api.icndb.com/jokes/random", true);
+  let query = document.getElementById("git-query").value;
+  xhr.open("GET", "https://api.github.com/search/repositories?q=" + query, true);
   xhr.send();
+}
+
+function useResponse(array) {
+    for(var i = 0; i < array.items.length; i++) {
+        let node = document.createElement("li");
+        let textNode = document.createTextNode(array.items[i].full_name);
+        node.appendChild(textNode);
+        document.getElementById("repo-list").appendChild(node);
+    }
 }
