9f43f3638d290a99c7b2d662a7e20742e9595b4f
guillermina <guilleves@gmail.com>
2016-09-24 18:58:29 -0300

    [Guillermina Vescovo] - Exercise 9 - Complete T02E09, issues with import statement -- ask after feedback

---
+++ 02-oop-inheritance/ex9.js
@@ -0,0 +1,38 @@
+import * as Actor from 'modules/actor';
+import * as EventEmitter from 'modules/event_emitter';
+import * as Logger from 'modules/logger';
+import * as Movie from 'modules/movie';
+
+let Social = {
+  share: function(friendName){
+    console.log("share", this.title, "with", friendName);
+  },
+  like: function(friendName){
+    console.log(friendName, "likes", this.title);
+  }
+}
+// let logger = new Logger();
+let gladiator = new Movie("gladiator", 1998, 120);
+let fightclub = new Movie("fight club", 1998, 120);
+let grandhotel = new Movie("grand hotel budapest", 2015, 130);
+
+// fightclub.on('play', logger.log)
+fightclub.on('play', function() {fightclub.play});
+fightclub.on('play', function() {console.log("hola")});
+fightclub.on('play', function() {console.log("2do listener")});
+// console.log(fightclub.listeners);
+// fightclub.play();
+//fightclub = Object.assign(Social, fightclub);
+// fightclub.share('guille');
+// fightclub.like("mariano");
+
+let cast = [
+    new Actor('Brad', 'Pitt', 'Shawnee, US'),
+    new Actor('Edward', 'Norton', 'Boston, US'),
+    new Actor('Helena', 'Bonham Carter', 'Islington, UK')
+];
+
+//console.log(cast);
+console.log(fightclub);
+fightclub.addCast(cast);
+console.log(fightclub);
