103f60cb4855f2d851a614bcdca8be89465a635a
Octavio Bruzzesi <octavio.bruzzesi@globant.com>
2016-10-07 16:28:34 -0300

    Octavio Bruzzesi, Topic 1: Exercise 6 ready.

---
+++ 01-JS-DOM-APIs/js/js.js
@@ -0,0 +1,27 @@
+function messageFirst() {
+  setTimeout(function () {
+    document.getElementById("content").innerHTML = "Hello world";
+    //let a = document.getElementById("content");
+    // a.style.display
+    // return("Hello world");
+  }, 2000);
+}
+function api(){
+
+  a.open(GET,"//api.icndb.com/jokes/random",false);
+  document.getElementById("joke").innerHTML = "aaa";
+}
+function eventRequest() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.open('GET', 'http://api.icndb.com/jokes/random', true);
+  xhttp.onreadystatechange = function(event) {
+    var response = JSON.parse(event.target.response);
+    document.getElementById('joke').innerHTML = response.value.joke;
+     if (xhttp.readyState === XMLHttpRequest.DONE) {
+       if (xhttp.responseText.value) {
+           document.getElementById("buttonSection").innerHTML  = xhttp.responseText.value.joke;
+       }
+     }
+   }
+ xhttp.send();
+}
