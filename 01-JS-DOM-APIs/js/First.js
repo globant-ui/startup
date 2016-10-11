24f453650c534362629aa6f51aa2d07d16f5d7f7
Octavio Bruzzesi <octavio.bruzzesi@globant.com>
2016-10-11 17:56:19 -0300

    Octavio Bruzzesi, Topic 1: Exercise 8 done, 9 started.

--- 01-JS-DOM-APIs/js/First.js
+++ 01-JS-DOM-APIs/js/First.js
@@ -6,40 +6,66 @@ function messageFirst() {
     // return("Hello world");
   }, 2000);
 }
+function git(){
+  let config = {
+    url: 'https://api.github.com/search/repositories'
+  }
+  eventsRequest(config);
+}
 
 function buttonClick(){
- var config = {
-   url: 'http://api.icndb.com/jokes/random'
-};
- function resolve(xhttp) {
+  let config = {
+    url: 'http://api.icndb.com/jokes/random'
+  }
+  eventsRequest(config)
+}
+function eventsRequest(config){
+
+  function resolve(xhttp) {
      var response = JSON.parse(event.target.response);
      document.getElementById('joke').innerHTML = response.value.joke;
-     if (xhttp.readyState === XMLHttpRequest.DONE)
-       {
-         if (xhttp.responseText.value)
-           {
-             document.getElementById("buttonSection").innerHTML  = xhttp.responseText.value.joke;
-           }
-       }
-};
- function reject() { };
- eventRequestReusable(config).then(resolve,reject);
+    //  if (xhttp.readyState === XMLHttpRequest.DONE){
+    //    if (xhttp.responseText.value){
+    //          document.getElementById("buttonSection").innerHTML  = xhttp.responseText.value.joke;
+    //     }
+    //   }
+    }
+
+  function resolveA(xhttp) {
+     var response = JSON.parse(event.target.response);
+     console.log("resolvea");
+     document.getElementById('joke').innerHTML = response.errors[0].field;
+   }
+
+   function reject() {
+     document.getElementById("sec").style.border = "3px solid red";
+   }
+
+   api(config).then(resolve, resolveA, reject);
 
 };
-function eventRequestReusable(object, resolve, reject) {
- return new Promise( function (resolve, reject) {
- var xhttp = new XMLHttpRequest();
- xhttp.open('GET', object.url , true);
- xhttp.send();
- xhttp.onload = function () {
-   if (this.status == 200) {
-     resolve(xhttp);
-   } else {
+function api(object, resolve, resolveA, reject) {
+   return new Promise( function (resolve, resolveA, reject) {
+   let xhttp = new XMLHttpRequest();
+   xhttp.open('GET', object.url , true);
+   console.log(xhttp);
+   xhttp.send();
+   xhttp.onload = function () {
+     if (this.status == 200) {
+       resolve(xhttp);
+       console.log("aaa");
+     } else {
+       if(this.status == 422){
+         console.log("if");
+         resolveA(xhttp);
+       }
+       else{
+         reject(this.statusText);
+      }
+     }
+   };
+   xhttp.onerror = function () {
      reject(this.statusText);
-   }
- };
- xhttp.onerror = function () {
-   reject(this.statusText);
- };
-})
+   };
+  })
 };
