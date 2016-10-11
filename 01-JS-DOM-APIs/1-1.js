78ee2c24904392ba9ae76fff68fceb6b731a33dd
Marcos Greppi <marcos.greppi@globant.com>
2016-10-11 17:58:49 -0300

    Marcos Greppi Topic1 finished up to 8, started 9

--- 01-JS-DOM-APIs/1-1.js
+++ 01-JS-DOM-APIs/1-1.js
@@ -1,7 +1,71 @@
-function asd() {
+function show() {
   let obj = document.getElementById("hidden");
   obj.style.display = "block";
 }
-function qwer() {
 
+function joke() {
+  let xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
+  xhttp.onreadystatechange = function(event) {
+    let response = JSON.parse(event.target.response);
+    document.getElementById("sectionJoke").innerHTML = response.value.joke;
+    if (xhttp.readyState === XMLHttpRequest.DONE) {
+      if (xhttp.responseText.value) {
+        document.getElementById("buttonJoke").innerHTML  = xhttp.responseText.value.joke;
+        }
+      }
+    }
+    xhttp.send();
+}
+
+function ajaxcall(){
+    let config = {
+      url: "http://api.icndb.com/jokes/random"
+    };
+    function resolve(xhttp) {
+      let response = JSON.parse(event.target.response);
+      document.getElementById("sectionAjax").innerHTML = response.value.joke;
+      if (xhttp.readyState === XMLHttpRequest.DONE) {
+        if (xhttp.responseText.value) {
+          document.getElementById("buttonAjax").innerHTML  = xhttp.responseText.value.joke;
+        }
+      }
+    };
+    function reject() {
+       document.getElementById("hidden").style.backgroundColor="red";
+    };
+    requestreusable(config).then(resolve,reject);
+  };
+
+function requestreusable(object, resolve, reject) {
+  return new Promise( function (resolve, reject) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", object.url , true);
+    xhttp.send();
+    xhttp.onload = function () {
+      if (this.status == 200) {
+        resolve(xhttp);
+      } else {
+        reject(this.statusText);
+      }
+    };
+    xhttp.onerror = function () {
+      reject(this.statusText);
+    };
+  })
+}
+
+function Qsearch() {
+  let xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "https://api.github.com/search/repositories", true);
+  xhttp.onreadystatechange = function(event) {
+    let response = JSON.parse(event.target.response);
+    document.getElementById("sectionQSearch").innerHTML = response.value.joke;
+    if (xhttp.readyState === XMLHttpRequest.DONE) {
+      if (xhttp.responseText.value) {
+        document.getElementById("buttonQSearch").innerHTML  = xhttp.responseText.value.joke;
+        }
+      }
+    }
+    xhttp.send();
 }
