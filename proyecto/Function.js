c15b43ade865267c095fc08c40a572d049b86901
DanoFKU <Danilofp13@gmail.com>
2016-10-18 16:22:44 -0300

    changes function.js
    add css div article{}

--- proyecto/Function.js
+++ proyecto/Function.js
@@ -1,5 +1,5 @@
 function showAllMovie()
-{for (var i = 0; i <= 10; i++) {
+{for (var i = 0; i <= objetojson.Peliculas.length; i++) {
            var poster =objetojson.Peliculas[i].poster;
         document.write("<li><a href=films/"+i+".html><img class=movi src="+ poster +" /></li><a>");}
     }
@@ -46,7 +46,7 @@ function Filt()
 }
 function ShowArr(ids)
 {
-	for(var i = 0;i<=10;i++)
+	for(var i = 0;i<=objetojson.Peliculas.length;i++)
 	{
 		if(i==ids)
 		{
@@ -60,8 +60,10 @@ function ShowArr(ids)
 		var plot = objetojson.Peliculas[i].plot;
 		var poster = objetojson.Peliculas[i].poster;
 		var rating= objetojson.Peliculas[i].rating;
-		document.write("<div><li> Nro "+(Id)+" <h1>"+Title+ "</h1> <li>Rating</li> "+rating+"</br></li><li><img class=movi src="+poster+" /></li><li><p>"+year+" </p></li><li><p>"+Release+"</p></li><li><h3>"+genere+"</h3></li><li><h2>"+director+"</h2></li><li><h3>"+actors+"</h3></li><li><article>"+plot+"</article></li></div></br>");
+		document.write("<div><li> Nro "+(Id)+" <h1>"+Title+ "</h1> <li>Rating</li> "+rating+"</br></li><li><img class=movi src="+poster+" /></li><li><p>"+year+" </p></li><li><p>"+Release+"</p></li><li><h3>"+genere+"</h3></li><li><h2>"+director+"</h2></li><li><h3>"+actors+"</h3></li><li></div><div><article>"+plot+"</article></li></div></br>");
 	    }
 	}
 	
 }
+
+
