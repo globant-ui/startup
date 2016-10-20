4e88bdba0ca1d099960e3dca58a0c000fa96224f
DANOFKU <Danilofp13@gmail.com>
2016-10-19 23:28:52 -0300

    done
    new functions that not found

--- proyecto/Function.js
+++ proyecto/Function.js
@@ -1,20 +1,20 @@
 function showAllMovie()
-{for (var i = 0; i <= objetojson.Peliculas.length; i++) {
-           var poster =objetojson.Peliculas[i].poster;
+{for (var i = 0; i <= objetojson.length; i++) {
+           var poster =objetojson[i].poster;
         document.write("<li><a href=films/"+i+".html><img class=movi src="+ poster +" /></li><a>");}
     }
 function showPopularMovie()
  {
- 		document.write("<li><a href=films/"+1+".html><img class=movi  src="+ objetojson.Peliculas[1].poster +" /></li><a>");
- 		document.write("<li><a href=films/"+2+".html><img class=movi  src="+ objetojson.Peliculas[2].poster +" /></li><a>");
- 		document.write("<li><a href=films/"+4+".html><img class=movi  src="+ objetojson.Peliculas[4].poster +" /></li><a>");
- 		document.write("<li><a href=films/"+6+".html><img class=movi  src="+ objetojson.Peliculas[6].poster +" /></li><a>");
+ 		document.write("<li><a href=films/"+1+".html><img class=movi  src="+ objetojson[1].poster +" /></li><a>");
+ 		document.write("<li><a href=films/"+2+".html><img class=movi  src="+ objetojson[2].poster +" /></li><a>");
+ 		document.write("<li><a href=films/"+4+".html><img class=movi  src="+ objetojson[4].poster +" /></li><a>");
+ 		document.write("<li><a href=films/"+6+".html><img class=movi  src="+ objetojson[6].poster +" /></li><a>");
  	}
 function showPremieresMovie()
  {
- 		document.write("<li><a href=films/"+1+".html><img class=movi  src="+ objetojson.Peliculas[1].poster +" /></li><a>");
- 		document.write("<li><a href=films/"+5+".html><img class=movi  src="+ objetojson.Peliculas[5].poster +" /></li><a>");
- 		document.write("<li><a href=films/"+6+".html><img class=movi  src="+ objetojson.Peliculas[6].poster +" /></li><a>");
+ 		document.write("<li><a href=films/"+1+".html><img class=movi  src="+ objetojson[1].poster +" /></li><a>");
+ 		document.write("<li><a href=films/"+5+".html><img class=movi  src="+ objetojson[5].poster +" /></li><a>");
+ 		document.write("<li><a href=films/"+6+".html><img class=movi  src="+ objetojson[6].poster +" /></li><a>");
  	}
 function Filt()
 {
@@ -46,20 +46,20 @@ function Filt()
 }
 function ShowArr(ids)
 {
-	for(var i = 0;i<=objetojson.Peliculas.length;i++)
+	for(var i = 0;i<=objetojson.length;i++)
 	{
 		if(i==ids)
 		{
-		var Id = objetojson.Peliculas[i].id;
-		var Title= objetojson.Peliculas[i].title;
-		var year = objetojson.Peliculas[i].year;
-		var Release = objetojson.Peliculas[i].released;
-		var genere = objetojson.Peliculas[i].genre;
-		var director = objetojson.Peliculas[i].director;
-		var actors = objetojson.Peliculas[i].actors;
-		var plot = objetojson.Peliculas[i].plot;
-		var poster = objetojson.Peliculas[i].poster;
-		var rating= objetojson.Peliculas[i].rating;
+		var Id = objetojson[i].id;
+		var Title= objetojson[i].title;
+		var year = objetojson[i].year;
+		var Release = objetojson[i].released;
+		var genere = objetojson[i].genre;
+		var director = objetojson[i].director;
+		var actors = objetojson[i].actors;
+		var plot = objetojson[i].plot;
+		var poster = objetojson[i].poster;
+		var rating= objetojson[i].rating;
 		document.write("<div><li> Nro "+(Id)+" <h1>"+Title+ "</h1> <li>Rating</li> "+rating+"</br></li><li><img class=movi src="+poster+" /></li><li><p>"+year+" </p></li><li><p>"+Release+"</p></li><li><h3>"+genere+"</h3></li><li><h2>"+director+"</h2></li><li><h3>"+actors+"</h3></li><li></div><div><article>"+plot+"</article></li></div></br>");
 	    }
 	}
@@ -67,12 +67,47 @@ function ShowArr(ids)
 }
 function showNLastMovie(ids)
 {
+	var a=objetojson.sort(function (a, b){
+    return (b.released - a.released)
+})
 	var j=1;
-	for (var i = objetojson.Peliculas.length-1; j <= ids; i--) 
+	for (var i = a.length-1; j <= ids; i--) 
 	{
-        var poster =objetojson.Peliculas[i].poster;
+        var poster =a[i].poster;
         document.write("<li><a href=films/"+i+".html><img class=movi src="+ poster +" /></li><a>");
         j++;
     }
 }
-
+function search()
+{
+	var b = document.getElementById("textBox").value;
+	var arr=objetojson;
+	var i =arr.indexOf(b);
+	    var Id = objetojson[i].id;
+		var Title= objetojson[i].title;
+		var year = objetojson[i].year;
+		var Release = objetojson[i].released;
+		var genere = objetojson[i].genre;
+		var director = objetojson[i].director;
+		var actors = objetojson[i].actors;
+		var plot = objetojson[i].plot;
+		var poster = objetojson[i].poster;
+		var rating= objetojson[i].rating;
+	document.write("<div><li> Nro "+(Id)+" <h1>"+Title+ "</h1> <li>Rating</li> "+rating+"</br></li><li><img class=movi src="+poster+" /></li><li><p>"+year+" </p></li><li><p>"+Release+"</p></li><li><h3>"+genere+"</h3></li><li><h2>"+director+"</h2></li><li><h3>"+actors+"</h3></li><li></div><div><article>"+plot+"</article></li></div></br>");
+}
+function AddFilm(tit,actor,director,uimg,dat,cat,poin,desc)
+{
+	tit=document.getElementById("Title1").value;
+	actor=document.getElementById("actor").value;
+	director=document.getElementById("director").value;
+	uimg=document.getElementById("urlimg").value;
+	dat=document.getElementById("Date").value;
+	cat=document.getElementById("Category").value;
+	poin=document.getElementById("pto").value;
+	desc=document.getElementById("mens").value;
+	objetojson["Title"]=tit;
+	objetojson["actors"]=actor;
+	objetojson["director"]=director;
+	objetojson["poster"]=uimg;
+	
+}
