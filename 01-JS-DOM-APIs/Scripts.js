2a882efbec0545308fa59442511a8092116b1fbf
Emmanuel de Arzave <manuarzave@gmail.com>
2017-03-07 14:00:28 -0300

    Topic1 ultimos ejercicios

--- 01-JS-DOM-APIs/Scripts.js
+++ 01-JS-DOM-APIs/Scripts.js
@@ -4,13 +4,20 @@ function fadeIn() {
   document.getElementById("oculto").style.opacity = "0.5";
 };
 
-function makeRequest(method, url, showId) {
+function configObject (metodo, url){
+  var object = {};
+  object.metodo = metodo;
+  object.url = url;
+  return object;
+}
+
+function makeRequest(object) {
   return new Promise(function (resolve, reject) {
     let xhr = new XMLHttpRequest();
-    xhr.open(method, url);
+    xhr.open(object.metodo, object.url);
     xhr.onload = function () {
       if (this.readyState == 4 && this.status == 200) {
-        resolve(document.getElementById(showId).innerHTML = xhr.response);
+        resolve(xhr.response);
       } else {
         reject({
           status: this.status,
@@ -28,11 +35,36 @@ function makeRequest(method, url, showId) {
   });
 };
 
+function readJokes(){
+  let obj = configObject('GET', 'http://api.icndb.com/jokes/random');
+  makeRequest(obj).then(
+    function(value){       
+      let parseValue = JSON.parse(value);
+      document.getElementById('icndbmessage').innerHTML = parseValue.value.joke;},
+    function(reason){
+      document.getElementById('icndbmessage').innerHTML = "error 404";
+      document.getElementById('oculto').style = "color:red";}
+  );
+};
 
+function searchinaRepository(input) {
+  let obj = configObject('GET', 'https://api.github.com/search/repositories?q='+input);
+  makeRequest(obj).then(
+    function(value){
+      let parseValue = JSON.parse(value);
+      console.log(parseValue.items);
+      for (let i = parseValue.items.length - 1; i >= 0; i--) {
+        let elementList = document.createElement('li');
+        let name = document.createTextNode(parseValue.items[i].full_name);
+        elementList.appendChild(name);
+        document.getElementById('lista').appendChild(elementList);
+      }},
+    function(reason) {
+      // do nothing
+  });
+}
 
-
-/*
-xmlhttprequest whitout promise
+/*xmlhttprequest whitout promise
 
 function get_icndbResponse(url){
   let xhttp = new XMLHttpRequest();
@@ -43,6 +75,4 @@ function get_icndbResponse(url){
   };
   xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
   xhttp.send();
-};
-
-*/
+};*/
