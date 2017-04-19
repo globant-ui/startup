52d8e6951b75b42d0147d92d0257c09ca617e48d
Nicolas Cassulo <nicolascassulo@gmail.com>
2017-04-19 20:53:15 -0300

    done exercise 6, need to improve

--- 00-HTML-CSS-basics/js/action.js
+++ 00-HTML-CSS-basics/js/action.js
@@ -1,6 +1,11 @@
+/*Button function*/
 
-document.getElementById("aButton").addEventListener("click", displayDate);
+document.getElementById("aButton").addEventListener("click", jokeApiConnect);
 
-function displayDate() {
-    document.getElementById("h1").innerHTML = Date();
+function jokeApiConnect(){
+	var xhr = new XMLHttpRequest();
+
+	xhr.open("GET", "http://api.icndb.com/jokes/random",false)
+	xhr.send();
+	document.getElementById("mainSection").innerHTML=xhr.responseText;	//Improve, we need the joke content not the object
 }
