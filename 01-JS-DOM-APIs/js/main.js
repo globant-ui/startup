df1b66132e3f7488925f6128590eebc5fcf7d07a
lucas-avila <lucasrotelavila@gmail.com>
2017-11-20 22:08:35 -0300

    Exercise 6 - Fetch data from API.

--- 01-JS-DOM-APIs/js/main.js
+++ 01-JS-DOM-APIs/js/main.js
@@ -2,3 +2,18 @@ window.onload = function() {
   var section = document.getElementById('hidden');
   section.style.opacity = "1";
 }
+
+function random_joke() {
+  let joke = document.getElementById('joke');
+  const url = "http://api.icndb.com/jokes/random";
+
+  fetch(url)
+  .then( (resp) => resp.json()) 
+  .then(function(data) {
+    let joke_details = data.value;
+    joke.innerHTML = joke_details.joke;
+  })
+  .catch(function(error) { //Just in case the API has any trouble.
+    console.log(error);
+  })
+}
