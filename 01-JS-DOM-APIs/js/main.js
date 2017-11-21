d299744c9fc727932143022996b42723f4e6483d
leonelsignore1995ab <33671020+leonelsignore1995ab@users.noreply.github.com>
2017-11-21 11:12:03 -0300

    Add files via upload

--- 01-JS-DOM-APIs/js/main.js
+++ 01-JS-DOM-APIs/js/main.js
@@ -5,29 +5,22 @@ function fade()
 	title.style.opacity="0";
 }
 
-const READY_STATE_UNINITIALIZATED = 0;
-const READY_STATE_LOADING = 1;
-const READY_STATE_LOADED = 2;
-const READY_STATE_INTERACTIVE = 3;
-const READY_STATE_COMPLETE = 4;
-var peticion_http;
-
-if(window.XMLHttpRequest)
-	peticion_http = new XMLHttpRequest();	//Navegadores modernos
 function getJoke()
 {
-	peticion_http.onreadystatechange = showJoke;
-	peticion_http.open("GET", "http://api.icndb.com/jokes/random", true);
-	peticion_http.send(null);
-	function showJoke()
-	{
-  		if(peticion_http.readyState == READY_STATE_COMPLETE && peticion_http.status == 200)
-  		{
-  			let element = document.createElement("p");
-			let object = JSON.parse(peticion_http.responseText);
-  			let content = document.createTextNode(object.value.joke);
-  			element.appendChild(content);
-  			document.getElementById('sectionJoke').appendChild(element);
-  		}
+	var config = new Object();
+	config.url = "http://api.icndb.com/jokes/random";
+	config.type = "GET";
+	getInfo(config);
+}
+
+function showInfo()
+{
+  	if(peticion_http.readyState == READY_STATE_COMPLETE && peticion_http.status == 200)
+  	{
+  		let element = document.createElement("p");
+		let object = JSON.parse(peticion_http.responseText);
+		let content = document.createTextNode(object.value.joke);
+		element.appendChild(content);
+		document.getElementById('sectionJoke').appendChild(element);
 	}
 }
