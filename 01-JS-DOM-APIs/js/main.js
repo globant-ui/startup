80402a07481688374160e93041b93ba60cab0655
lucas-avila <lucasrotelavila@gmail.com>
2017-11-21 01:54:49 -0300

    JS - Exercise 7 (On development).

--- 01-JS-DOM-APIs/js/main.js
+++ 01-JS-DOM-APIs/js/main.js
@@ -17,3 +17,58 @@ function random_joke() {
     console.log(error);
   })
 }
+
+// Alternative with HXMLHttpRequest
+/*function random_joke2() {
+  let url = "http://api.icndb.com/jokes/random";
+  var myRequest = new XMLHttpRequest();
+
+  myRequest.onreadystatechange = function () { 
+    if (this.readyState === 4 && this.status == 200) {
+      let json = JSON.parse(this.responseText);
+      document.getElementById('joke').innerHTML = json.value.joke;
+    } else if(this.status == 400) {
+      alert("400 error");
+    }
+  };
+
+  myRequest.open('GET', url);
+  myRequest.send();
+}*/
+
+// Exercise 7 -- Still testing | Does not work.
+
+function random_joke2() {
+  let joke = document.getElementById('joke');
+  const url = "http://api.icndb.com/jokes/random";
+  get_http(url)
+  .then(function(value) {
+    let json = JSON.parse(value);
+    joke.innerHTML = json.value.joke;
+  },
+  function (reason) {
+    console.error('Something went wrong', reason);
+  });
+}
+
+function get_http(url) {
+  return new Promise(function (resolve, reject) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+      if (this.status === 200) {
+        resolve(this.responseText);
+      } else {
+        reject(new Error(this.statusText));
+      }
+    }
+
+    request.onerror = function () {
+      reject(new Error(
+        'XMLHttpRequest Error: '+this.statusText));
+    };
+
+    request.open('GET', url);
+    request.send();    
+  });
+}
+
