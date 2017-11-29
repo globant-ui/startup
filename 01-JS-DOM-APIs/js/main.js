0256d01aea40f3a0358badb5121e6314d8b2a52b
lucas-avila <lucasrotelavila@gmail.com>
2017-11-28 22:03:53 -0300

    [Add] Exercise 12 | Changes some types to const or let.

--- 01-JS-DOM-APIs/js/main.js
+++ 01-JS-DOM-APIs/js/main.js
@@ -6,16 +6,17 @@ window.onload = function() {
   document.getElementById('joke-button').addEventListener("click", random_joke3, false);
   document.getElementById('repo-button').addEventListener("click", search_repositories, false);
   document.getElementById('repo-search').addEventListener("submit", search_repositories2, false);
+  document.getElementById('table-button').addEventListener("click", create_table, false);
 }
 
 function random_joke() {
-  let joke = document.getElementById('joke');
+  const joke = document.getElementById('joke');
   const url = "http://api.icndb.com/jokes/random";
 
   fetch(url)
   .then( (resp) => resp.json()) 
   .then(function(data) {
-    let joke_details = data.value;
+    const joke_details = data.value;
     joke.innerHTML = joke_details.joke;
   })
   .catch(function(error) { //Just in case the API has any trouble.
@@ -25,12 +26,12 @@ function random_joke() {
 
 // Exercise 6 -- Alternative with XMLHttpRequest
 function random_joke2() {
-  let url = "http://api.icndb.com/jokes/random";
+  const url = "http://api.icndb.com/jokes/random";
   var myRequest = new XMLHttpRequest();
 
   myRequest.onreadystatechange = function () { 
     if (this.readyState === 4 && this.status == 200) {
-      let json = JSON.parse(this.responseText);
+      const json = JSON.parse(this.responseText);
       document.getElementById('joke').innerHTML = json.value.joke;
     } else if(this.status == 400) {
       alert("400 error");
@@ -44,10 +45,10 @@ function random_joke2() {
 // Exercise 7
 
 function random_joke3() {
-  let joke = document.getElementById('joke');
+  const joke = document.getElementById('joke');
 
   /* Now we create a config object, just as an example.*/
-  let config = {
+  const config = {
     url: "http://api.icndb.com/jokes/random"
   }
   /*Then we just delegate the responsability to the promise */
@@ -83,7 +84,7 @@ function get_http(config) {
 
 // Exercise 9
 function search_repositories(){
-  let config = {
+  const config = {
     url: "https://api.github.com/search/repositories?q='JavaScript'"
   }
 
@@ -104,7 +105,7 @@ function get_repositories(config){
 function generate_list(response) {
 
   document.getElementById("repositories").innerHTML = ""; // Empty the div before filling it again.
-  var items = JSON.parse(response).items;
+  const items = JSON.parse(response).items;
 
   items.forEach(function(item) {
     create_list_item(item);
@@ -112,11 +113,11 @@ function generate_list(response) {
 }
 
 function create_list_item(item) {
-  let paragraph = document.createElement("P");                  
-  let text = document.createTextNode(item.full_name);
+  const paragraph = document.createElement("P");                  
+  const text = document.createTextNode(item.full_name);
   paragraph.appendChild(text);                                       
   document.getElementById("repositories").appendChild(paragraph); 
-  var ul = document.createElement("UL");
+  const ul = document.createElement("UL");
   paragraph.appendChild(ul);
   
   for(let property in item){
@@ -125,8 +126,8 @@ function create_list_item(item) {
 }
 
 function create_ul_elements(property, value, father) {
-  let list_item = document.createElement("LI");                  
-  let text = document.createTextNode(property + ": " + value);
+  const list_item = document.createElement("LI");                  
+  const text = document.createTextNode(property + ": " + value);
   list_item.appendChild(text);                                         
   father.appendChild(list_item); 
 }
@@ -134,9 +135,40 @@ function create_ul_elements(property, value, father) {
 // Exercise 10
 function search_repositories2(form){
   form.preventDefault();
-  let input = document.getElementById("searchParameter").value;
-  let config = {
+  const input = document.getElementById("searchParameter").value;
+  const config = {
     url: "https://api.github.com/search/repositories?q='" + input + "'"
   }
   get_repositories(config);
+}
+
+//Exercise 12
+
+function create_table(){
+  let table = [
+  ["Movie title", "$$$", "Main Character", "Like | Dislike"],
+  ["Titanic", "3 Billon U$D", "Brad Pitt", "Like"],
+  ["Jaws", "1 Billon U$D", "A shark", "No comments"],
+  ["Star Wars", "3 TRILLON!!", "Rey", "Super - Like"]
+  ]
+ // Warning: the matrix must be NxN. 
+  create_table_elements(table);
+}
+
+function create_table_elements(table){
+
+  const t = document.createElement("TABLE");
+
+  for(let i=0; i < table.length; i++){
+    const row = document.createElement("TR");
+    for(let j=0; j < table.length; j++){
+      const column = document.createElement("TD");
+      const text = document.createTextNode(table[i][j]);
+      column.appendChild(text);
+      row.appendChild(column);
+    }
+    t.appendChild(row);
+  }
+
+  document.getElementById("table").appendChild(t);
 }
