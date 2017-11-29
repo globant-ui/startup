ec44130e84683f01eba9de7f716b5c37117c95a1
leonelsignore1995ab <33671020+leonelsignore1995ab@users.noreply.github.com>
2017-11-29 12:10:32 -0300

    Add files via upload

--- 01-JS-DOM-APIs/Repositories/js/main.js
+++ 01-JS-DOM-APIs/Repositories/js/main.js
@@ -8,25 +8,32 @@ function search() {
 		method: "GET",
 		url: "https://api.github.com/search/repositories" + "?q=" + word.value
 	};
-	ajaxCall(config);
-}
 
-function showInfo(xhr) {
-	console.log(xhr.responseText);
-	var list = document.getElementById("list");
-	if (list.hasChildNodes()) {
-		while (list.childNodes.length >= 1) {
-			list.removeChild(list.firstChild);
+	var prom = ajaxCall(config);
+
+	prom.then(function (txt) {
+		console.log(txt);
+		var list = document.getElementById("list");
+		if (list.hasChildNodes()) {
+			while (list.childNodes.length >= 1) {
+				list.removeChild(list.firstChild);
+			}
 		}
-	}
-	var obj = JSON.parse(xhr.response);
-	if(obj.total_count == 0) {
-		alert("No matches found");
-	}
-	for(var i = 0; i < obj.items.length; i++) {
-		let element = document.createElement("li");
-		let content = document.createTextNode(obj.items[i].full_name);
-		element.appendChild(content);
-		document.getElementById("list").appendChild(element);
-	}
+		var obj = JSON.parse(txt);
+		if(obj.total_count == 0) {
+			alert("No matches found");
+		}
+		for(var i = 0; i < obj.items.length; i++) {
+			let element = document.createElement("li");
+			let content = document.createTextNode(obj.items[i].full_name);
+			element.appendChild(content);
+			document.getElementById("list").appendChild(element);
+		}
+	});
+
+	prom.catch(function () {
+		alert("Sorry, there was an error making the request");
+	});
+
+	
 }
