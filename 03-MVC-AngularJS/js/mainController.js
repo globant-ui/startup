b0a0f58b95418c431cde19bc07c1ec886e6ee7f1
leonelsignore1995ab <33671020+leonelsignore1995ab@users.noreply.github.com>
2017-12-11 19:33:35 -0300

    Add files via upload

--- 03-MVC-AngularJS/js/mainController.js
+++ 03-MVC-AngularJS/js/mainController.js
@@ -1,6 +1,8 @@
 angular.module("moviesApp", [])
 .controller("mainController", function ($scope) {
 	$scope.movieList = movieArray;
+	$scope.showDetails = false;
+	$scope.editMode = false;
 	$scope.addMovie = function () {
 		$scope.movieList.push($scope.newMovie);
 		$scope.newMovie = {};
@@ -8,23 +10,27 @@ angular.module("moviesApp", [])
 	$scope.clearAll = function () {
 		$scope.movieList = [];
 	};
+	$scope.details = function () {
+		if(!$scope.showDetails) {
+			$scope.showDetails = true;
+		}
+		else {
+			$scope.showDetails = false;
+		}
+	};
 	$scope.editMovie = function (movieToEdit) {
 		$scope.newMovie = movieToEdit;
 		document.getElementById('btnFinish').style.visibility="visible";
 		document.getElementById('btnSubmit').style.visibility="hidden";
 		document.getElementById('btnClr').style.visibility="hidden";
-		/*document.getElementById('details').style.visibility="hidden";
-		document.getElementById('edit').style.visibility="hidden";
-		document.getElementById('remove').style.visibility="hidden";*/
+		$scope.editMode = true;
 	};
 	$scope.finishEdit = function () {
 		$scope.newMovie = {};
 		document.getElementById('btnFinish').style.visibility="hidden";
 		document.getElementById('btnSubmit').style.visibility="visible";
 		document.getElementById('btnClr').style.visibility="visible";
-		/*document.getElementById('details').style.visibility="visible";
-		document.getElementById('edit').style.visibility="visible";
-		document.getElementById('remove').style.visibility="visible";*/
+		$scope.editMode = false;
 	};
 	$scope.removeMovie = function (movieToRemove) {
 		$scope.movieList = $scope.movieList.filter(function (movieToCompare) {
