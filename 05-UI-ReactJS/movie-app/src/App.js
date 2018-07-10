05cd026efe7ce9a4d10a0c3dbb8b4601de3a556a
Julian Estefan <julian.y.estefan@gmail.com>
2018-07-09 21:49:34 -0300

    Movie list began.

--- 05-UI-ReactJS/movie-app/src/App.js
+++ 05-UI-ReactJS/movie-app/src/App.js
@@ -2,31 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-function MovieData(props) {
-  const movie = props.movie;
+function MovieDataRow(props) {
   return (
-    <table>
-      <td>{movie.tittle}</td>
-      <td>{movie.year}</td>
-      <td>{movie.duration}</td>
-    </table>
+    <tr>
+      <td>{this.props.tittle}</td>
+      <td>{this.props.year}</td>
+      <td>{this.props.duration}</td>
+    </tr>
   );
 }
 
-/*
-class MovieCreator extends Componente {
+class MovieList extends Component {
+  constructor (props){
+    super(props);
+
+  }
+
   render() {
     return (
-      <fieldset>
-        <input type="text" value={props.movies.tittle}></input>
-        <input type="number" value={props.movies.year}></input>
-        <input type="time" value={props.movies.duration}></input>
-        <input type="buttom" value="add" onClick="this.props.handleClick" > Add Movie!! </button>
-      </fieldset>
-    );
+      <table>
+        <tr>
+          <th> Tittle </th>
+          <th> Year </th>
+          <th> Duration </th>
+        </tr>
+      </table>
+    )
   }
 }
-*/
 
 class App extends Component {
   constructor(props) {
@@ -39,32 +42,32 @@ class App extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    
+
     const tittle = e.target[0].value;
     const year = e.target[1].value;
     const duration = e.target[2].value;
 
     let newMovie = {
-      tittle: {tittle},
-      year: {year},
-      duration: {duration}
+      tittle: { tittle },
+      year: { year },
+      duration: { duration }
     };
 
     this.setState(prevState => ({
       movies: [...prevState.movies, newMovie]
     }));
-      
-    console.log(this.state.movies.tittle);
+
   }
 
   render() {
     return (
-      <form onSubmit = {this.handleSubmit}>
-        <input type="text" name = "tittle" placeholder="Tittle" ></input>
-        <input type="number" name = "year" min = "1920" max = "2018" placeholder="Year" ></input>
-        <input type="time" name = "duration"></input>
-        <button type= "submit"> Add Movie</button>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="tittle" placeholder="Tittle" ></input>
+        <input type="number" name="year" min="1920" max="2018" placeholder="Year" ></input>
+        <input type="time" name="duration"></input>
+        <button type="submit"> Add Movie</button>
       </form>
+      <MovieList movies = {this.state.movies} />
     );
   }
 }
