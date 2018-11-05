ce68aa78be0e9d58c8ab2acd64f6945617f303de
Matias <matias-190198@hotmail.com>
2018-11-05 02:29:24 -0300

    Adding reusable function(exercise 3)

--- 01-JS-DOM-APIs/main.js
+++ 01-JS-DOM-APIs/main.js
@@ -1,31 +1,58 @@
 
-//when the window is loaded "hello world" appears with a animation.
+//when the window is loaded "hello world" appears with a animation(Fade in).
+
 window.addEventListener("load",() => {
     let hdn = document.getElementsByClassName("hidden-text")[0];
     hdn.innerHTML = "Hello World!";
 });
 
-
-let btn =  document.getElementsByClassName("joke")[0];
-
-//add a event to the button
-btn.addEventListener("click",get_joke);
+ // ************************************************************************
+ // ************************************************************************
+ // ************************************************************************
 
 
-//With a XMLhttprequest get a inforamtion in text form from "http://api.icndb.com/jokes/random"   
-function get_joke(){
-    let xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://api.icndb.com/jokes/random", true);
-    xhttp.onreadystatechange = () => {
-        if(xhttp.readyState == 4 && xhttp.status == 200){
-            let myData = JSON.parse(xhttp.responseText); //passes from text form to json form
-            alert(myData.value.joke);
-        }
-    }
-    xhttp.send();
+//reusable_fun()
+//take:(string,string,bool) return: Promise
+//Gets the information from the taked API via XMLhttprequest
+function reusable_fun (mtd, url, asyn) {
+    let myPromise = new Promise( (resolve,reject) => {
+        let xhttp = new XMLHttpRequest();
+        xhttp.open(mtd, url, asyn);
+        xhttp.onreadystatechange = () => {
+            if(xhttp.readyState == 4 && xhttp.status == 200){
+                resolve(xhttp.responseText); 
+            } else {
+                reject;
+            }
+        };
+        xhttp.send();
+    });
+    return myPromise;
 }
 
+//Shows the joke if everything is right, and shows an error if something is wrong
+function show_joke(){
+    let jokePromise = reusable_fun("GET", "http://api.icndb.com/jokes/random", true);
+    jokePromise.then((txt)=>{
+        let myData = JSON.parse(txt); //passes from text form to json form
+        let jkc = document.getElementsByClassName("joke-text")[0];
+        jkc.innerText = myData.value.joke + "";
+    }).catch(() => {
+        let err = document.getElementsByClassName("joke-container")[0];
+        err.style.backgroundColor = "red";
+        document.getElementsByClassName("joke-text")[0].innerHTML = "ERROR";
+        err.style.animation = "fadeIn 2s";
+    });
+}
 
+//When you press the button "Get a Joke" show_joke is triggered
+document.getElementsByClassName("joke")[0].addEventListener("click",show_joke);
+
+ // ************************************************************************
+ // ************************************************************************
+ // ************************************************************************
+
+ //Gets the information from "https://api.github.com/search/repositories?q=" with parameters via XMLhttprequest
 function get_repositorie(){
     let xhr = new XMLHttpRequest();
     let search = document.getElementById("git").value;
@@ -34,12 +61,14 @@ function get_repositorie(){
     xhr.send();
 }
 
+
+//Shows the list of repositories in the right side of screen
 function show_list(user){
     let usrList = document.getElementsByClassName("user-list")[0];
     let usrUl = document.createElement("ul");
     for(let i = 0;i < user.items.length ; i++){
         let usrLi = document.createElement("li");
-        let usrRepo = document.createTextNode(user.items[i].name);
+        let usrRepo = document.createTextNode(user.items[i].name+" _");
         usrLi.appendChild(usrRepo);
         usrUl.appendChild(usrLi);
     }
