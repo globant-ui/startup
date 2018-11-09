d1f19b1ae3ed9de61f7066b8b73df74ff0e191ce
LLSaldias <llsaldiasdev@gmail.com>
2018-11-09 20:01:59 -0300

    error with edit and delete

--- 03-MVC-Angular/Movie-app/src/app/movie-list/movie-list.component.ts
+++ 03-MVC-Angular/Movie-app/src/app/movie-list/movie-list.component.ts
@@ -7,20 +7,35 @@ import { Movies } from '../models/movies';
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-
+  editShow = false;
   constructor() { }
   movieList: Movies[] = [
-    { id: 1, title: 'Avenger I', year: 2012, duration: 120},
-    { id: 2, title: 'Avenger II', year: 2015, duration: 120},
-    { id: 3 , title: 'Avenger III', year: 2018, duration: 120}
+    { id: 1, title: 'Avenger I', year: 2012, duration: 120 },
+    { id: 2, title: 'Avenger II', year: 2015, duration: 120 },
+    { id: 3, title: 'Avenger III', year: 2018, duration: 120 }
   ];
 
   selectedMovie: Movies = new Movies();
+  editedMovie: Movies = new Movies();
 
   addNew() {
     this.selectedMovie.id = this.movieList.length + 1;
     this.movieList.push(this.selectedMovie);
+    this.selectedMovie = new Movies();
+  }
+  openEdit(movie: Movies) {
+    this.editShow = true;
+    movie.title = this.editedMovie.title;
+    movie.year = this.editedMovie.year;
+    movie.duration = this.editedMovie.duration;
 
+  }
+  confirmEdit() {
+    this.editShow = false;
+
+  }
+  delete(movie: Movies) {
+    this.movieList = this.movieList.filter(x => x !== this.selectedMovie);
     this.selectedMovie = new Movies();
   }
 
