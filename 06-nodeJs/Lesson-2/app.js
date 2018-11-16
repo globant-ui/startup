649a9a99ceb65b5f98a2e6758be6e6a992a00a4d
LLSaldias <llsaldiasdev@gmail.com>
2018-11-16 11:03:59 -0300

    lesson 2 using promises

---
+++ 06-nodeJs/Lesson-2/app.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const getArg = require('./modules/consoleInput')
+
+let now = new Date();
+now_string = JSON.stringify(now);
+const file = './logger.log';
+const fsPromises = fs.promises;
+
+var user = getArg('--user');
+var psw = getArg('--psw');
+
+data = [
+  `usuario: ${user}`,
+  `contraseña: ${psw}`,
+  `Fecha y hora actual: ${now_string}`
+]
+
+
+fsPromises.access(file, fs.constants.R_OK | fs.constants.W_OK)
+  .then(() =>console.log('the log file alredy exist'),
+    fs.appendFile(file, data, (err) => {
+      if (err) throw err;
+    }),
+    console.log('...has been updated')
+    )
+
+  .catch(() =>
+    fs.writeFile(file, data, (err) => {
+      if (err) throw err;
+    }),
+    console.error('the log file has been created'));
