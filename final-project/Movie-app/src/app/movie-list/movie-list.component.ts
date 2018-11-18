165af35feed0043db1ffd55835da11725077f13d
LLSaldias <llsaldiasdev@gmail.com>
2018-11-18 14:34:18 -0300

    angular final project dir created

---
+++ final-project/Movie-app/src/app/movie-list/movie-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Movies } from '../models/movies';
+
+@Component({
+  selector: 'app-movie-list',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.css']
+})
+export class MovieListComponent implements OnInit {
+  editShow = false;
+  constructor() { }
+  movieList: Movies[] = [
+    { id: 1, title: 'Avenger I', year: 2012, duration: 120 },
+    { id: 2, title: 'Avenger II', year: 2015, duration: 120 },
+    { id: 3, title: 'Avenger III', year: 2018, duration: 120 }
+  ];
+
+  selectedMovie: Movies = new Movies();
+  editedMovie: Movies = new Movies();
+
+  addNew() {
+    this.selectedMovie.id = this.movieList.length + 1;
+    this.movieList.push(this.selectedMovie);
+    this.selectedMovie = new Movies();
+  }
+  openEdit(movie: Movies) {
+    this.editShow = true;
+    this.editedMovie = movie;
+  }
+  confirmEdit(movie: Movies) {
+    this.editShow = false;
+  }
+  delete(movie: Movies) {
+    this.movieList = this.movieList.filter(x => x !== movie);
+    movie = new Movies();
+  }
+
+  ngOnInit() {
+  }
+
+}
