ac6d2284b4b2880646ffefb7f6baf3c5242e7c5e
Roberto David Marcos <roberto.marcos@globant.com>
2018-12-04 14:56:22 -0300

    avance de JS

--- 01-JS-DOM-APIs/prueba.js
+++ 01-JS-DOM-APIs/prueba.js
@@ -1,4 +1,22 @@
-function mostrarSecreto(){
-	var x = document.getElementById("secret");
-	x.style.display = "block";
+function mostrarAnimacion() {
+	var x = document.getElementById("oculto");
+	x.style.opacity = 1;
 }
+
+
+
+function mostrarMensaje(){
+	alert("Mensaje: Hello world")
+}
+
+function getJoke(){
+	fetch('http://api.icndb.com/jokes/random').then(response => {
+	  return response.json();
+	}).then(data => {
+		joke= data.value.joke;
+		document.getElementById('destino').innerHTML = 'The joke: ' + joke;
+	}).catch(err => {
+	  	alert("Fallo la captura del JSON!")
+	})
+}
+
